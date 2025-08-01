ede90f64e7f2e980a05b323d08fdf0d9da4c201e
Mike Dawson <mike@ustadmobile.com>
2025-08-01 17:14:43 +0400

    Fix make subdomain bundle

--- server/make-subdomain-bundle.sh
+++ server/make-subdomain-bundle.sh
@@ -14,7 +14,7 @@ mkdir $OUTDIR
 cp /etc/letsencrypt/live/$SUBDOMAIN.$DOMAIN/fullchain.pem $OUTDIR
 cp /etc/letsencrypt/live/$SUBDOMAIN.$DOMAIN/privkey.pem $OUTDIR
 cp /etc/letsencrypt/options-ssl-apache.conf $OUTDIR
-sed "s/DOMAIN/$SUBDOMAIN.$DOMAIN/g" templates/002-localhttps.conf > $OUTDIR/002-localhttps-$DOMAIN.conf
+sed "s/DOMAIN/$SUBDOMAIN.$DOMAIN/g" templates/002-localhttps.conf > $OUTDIR/002-localhttps-$SUBDOMAIN.$DOMAIN.conf
 sed "s/DOMAIN/$SUBDOMAIN.$DOMAIN/g" templates/bundle-install.sh > $OUTDIR/install.sh
 
 zip -r localhttps-$SUBDOMAIN.$DOMAIN-bundle.zip $OUTDIR
