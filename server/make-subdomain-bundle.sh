ecdc774d6f07859eb002789a90c253ad6c62946d
Mike Dawson <mike@ustadmobile.com>
2025-08-01 17:09:46 +0400

    fixes for generation of local bundle maker

--- server/make-subdomain-bundle.sh
+++ server/make-subdomain-bundle.sh
@@ -2,15 +2,19 @@
 
 OUTDIR=localhttps-$DOMAIN
 
+#Local dev subdomain
+SUBDOMAIN=localdev
+
+
 if [ -e $OUTDIR ]; then
    rm -r $OUTDIR
 fi
 
 mkdir $OUTDIR
-cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem $OUTDIR
-cp /etc/letsencrypt/live/$DOMAIN/privkey.pem $OUTDIR
+cp /etc/letsencrypt/live/$SUBDOMAIN.$DOMAIN/fullchain.pem $OUTDIR
+cp /etc/letsencrypt/live/$SUBDOMAIN.$DOMAIN/privkey.pem $OUTDIR
 cp /etc/letsencrypt/options-ssl-apache.conf $OUTDIR
-sed "s/DOMAIN/$DOMAIN/g" templates/002-localhttps.conf > $OUTDIR/002-localhttps-$DOMAIN.conf
-sed "s/DOMAIN/$DOMAIN/g" templates/bundle-install.sh > $OUTDIR/install.sh
+sed "s/DOMAIN/$SUBDOMAIN.$DOMAIN/g" templates/002-localhttps.conf > $OUTDIR/002-localhttps-$DOMAIN.conf
+sed "s/DOMAIN/$SUBDOMAIN.$DOMAIN/g" templates/bundle-install.sh > $OUTDIR/install.sh
 
-zip -r localhttps-$DOMAIN-bundle.zip $OUTDIR
+zip -r localhttps-$SUBDOMAIN.$DOMAIN-bundle.zip $OUTDIR
